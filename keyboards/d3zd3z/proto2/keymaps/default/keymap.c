a616e2c147779e2768ae77dbed83e06869307bee
David Brown <davidb@davidb.org>
2022-11-03 09:34:48 -0600

    proto2: Implement the nav layer
    Pressing the leftmost left thumb pair selects nav mode (like shift).
    These are the navigation keys in the middle of the section on a full
    keyboard.

--- keyboards/d3zd3z/proto2/keymaps/default/keymap.c
+++ keyboards/d3zd3z/proto2/keymaps/default/keymap.c
@@ -8,6 +8,7 @@ enum layers {
 	STENO,
 	QWERTY,
         NUM,
+	NAV,
 };
 
 /* Definitions for the bottom row that can also be modifiers. */
@@ -26,15 +27,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         TO(QWERTY),   STN_S1, STN_TL, STN_PL, STN_HL, STN_ST1,   STN_ST3, STN_FR, STN_PR, STN_LR, STN_TR, STN_DR,
         KC_NO,        STN_S2, STN_KL, STN_WL, STN_RL, STN_ST2,   STN_ST4, STN_RR, STN_BR, STN_GR, STN_SR, STN_ZR,
                        STN_N1, STN_A, STN_O,   STN_E, STN_U, STN_N2),
+
     [QWERTY] = LAYOUT(
         TO(STENO),  KC_Q, KC_W, KC_E, KC_R, KC_T,   KC_Y, KC_U, KC_I,    KC_O,    KC_P,   KC_BSLS,
 	KC_NO,      BM_Z, BM_X, BM_C, BM_V, KC_B,   KC_N, BM_M, BM_COMM, BM_DOT, BM_SLSH, KC_ESC,
 	               MO(NUM), KC_TAB, KC_BSPC,    KC_ENT, KC_SPC, MO(NUM)),
+
     /* Numbers and some random punctuation.  The rest of the punction
      * can be defined as combos. */
     [NUM] = LAYOUT(
         KC_NO,      KC_1, KC_2, KC_3, KC_4, KC_5,    KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS,
         KC_NO,      LSFT(KC_1), LSFT(KC_2), LSFT(KC_3), LSFT(KC_4), LSFT(KC_5),    LSFT(KC_6), LSFT(KC_7), LSFT(KC_8), LSFT(KC_9), LSFT(KC_0), LSFT(KC_MINS),
+                   KC_NO, KC_NO, KC_NO,   KC_NO, KC_NO, KC_NO),
+
+    /* Navigation.  This encodes the middle navigation keys on a full
+     * keyboard. */
+    [NAV] = LAYOUT(
+	KC_NO,      KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,           KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_PAUS, KC_NO,
+	KC_NO,      KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, KC_NO,   KC_PSCR, KC_BSPC, KC_DEL,  KC_SCRL, KC_NO, KC_NO,
                    KC_NO, KC_NO, KC_NO,   KC_NO, KC_NO, KC_NO),
 };
 
@@ -49,7 +59,7 @@ const uint16_t PROGMEM cmb_j[] = {KC_U, BM_M, COMBO_END};
 const uint16_t PROGMEM cmb_k[] = {KC_I, BM_COMM, COMBO_END};
 const uint16_t PROGMEM cmb_l[] = {KC_O, BM_DOT, COMBO_END};
 const uint16_t PROGMEM cmb_scln[] = {KC_P, BM_SLSH, COMBO_END};
-const uint16_t PROGMEM cmb_quot[] = {KC_KC_BSLS, KC_ESC, COMBO_END};
+const uint16_t PROGMEM cmb_quot[] = {KC_BSLS, KC_ESC, COMBO_END};
 
 /* In addition, borrow various symbols from the Emily's symbols.
  * These work in qwerty mode, and are the same basic as they would be
@@ -91,6 +101,15 @@ const uint16_t PROGMEM cmb_colon[] = {KC_B, KC_O, BM_DOT, COMBO_END};
 const uint16_t PROGMEM cmb_semicolon[] = {KC_B, BM_M, BM_COMM, COMBO_END};
 const uint16_t PROGMEM cmb_question[] = {KC_B, KC_U, KC_I, BM_COMM, COMBO_END};
 const uint16_t PROGMEM cmb_underscore[] = {KC_B, BM_COMM, BM_DOT, COMBO_END};
+
+/* Combinations for the thumb keys for the other layers. */
+const uint16_t PROGMEM cmb_navmdoe[] = {MO(NUM), KC_TAB, COMBO_END};
+
+/* Navmode combos for the middle buttons. */
+const uint16_t PROGMEM cmb_nav_left[] = {KC_HOME, KC_PSCR, COMBO_END};
+const uint16_t PROGMEM cmb_nav_down[] = {KC_PGDN, KC_BSPC, COMBO_END};
+const uint16_t PROGMEM cmb_nav_up[] = {KC_PGUP, KC_DEL, COMBO_END};
+const uint16_t PROGMEM cmb_nav_right[] = {KC_END, KC_SCRL, COMBO_END};
 
 combo_t key_combos[] = {
     COMBO(cmb_a, KC_A),
@@ -140,6 +159,15 @@ combo_t key_combos[] = {
     COMBO(cmb_semicolon, KC_SCLN),
     COMBO(cmb_question, LSFT(KC_SLSH)),
     COMBO(cmb_underscore, LSFT(KC_MINS)),
+
+    /* Layer selects via thumbs. */
+    COMBO(cmb_navmdoe, MO(NAV)),
+
+    /* Navmode combos. */
+    COMBO(cmb_nav_left, KC_LEFT),
+    COMBO(cmb_nav_down, KC_DOWN),
+    COMBO(cmb_nav_up, KC_UP),
+    COMBO(cmb_nav_right, KC_RIGHT),
 };
 uint16_t COMBO_LEN = sizeof(key_combos) / sizeof(key_combos[0]);
 
@@ -156,6 +184,12 @@ layer_state_t layer_state_set_user(layer_state_t state) {
     case QWERTY:
         SET_LED_QWERTY();
         break;
+    case NUM:
+        rgblight_setrgb(16, 64, 16);
+	break;
+    case NAV:
+        rgblight_setrgb(16, 64, 64);
+	break;
     default:
         rgblight_setrgb(64, 16, 16);
         break;
